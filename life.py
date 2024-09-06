8a11c572c456dca9bf05a2b59e6428fa0c165cfe
baynobu <darmawanhanif30@gmail.com>
2024-09-06 13:00:35 +0700

    initial commit

---
+++ life.py
@@ -0,0 +1,12 @@
+import math
+
+theAnswer = 42
+
+def quote_marvin():
+    # Use \ for clear the effect
+    # e.g 'i don\'t do it anymore.', or you can use "" instead ''
+    print('I\'ve calculate your chance of survival, but I don\'t think youll like it.')
+    
+def square_root(x):
+    # print(math.sqrt(63.14))
+    return math.sqrt(x)
